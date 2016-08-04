2f8f161be4ba33185250f0f2314bb30e225f5674
Julian Brown <julian.brown1@dwp.gsi.gov.uk>
2016-08-04 15:35:36 +0100

    Fixed style warnings

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -2,20 +2,34 @@ package pro.jtaylor.timetracker.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 import pro.jtaylor.timetracker.core.dao.TimeEntry;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/***
+ * @author Jason
+ * @since 2015-08-03
+ */
 @Component
 public class Tracker {
+    /***
+     * ArrayList of timeEntry objects that compose the timesheet
+     */
     @Autowired
     private List<TimeEntry> entries;
 
     public Tracker() {
-
+        entries = new ArrayList<>();
     }
 
+    /***
+     * Adds entry to timesheet
+     * @param entry timesheet entry to add to tracker timesheet
+     */
     void add(TimeEntry entry) {
+        Assert.notNull(entry);
         entries.add(entry);
     }
 
@@ -23,6 +37,10 @@ public class Tracker {
 //        entries.remove(entry);
 //    }
 
+    /***
+     * Gets number of entries in timesheet
+     * @return entries in timesheet
+     */
     int size() {
         return entries.size();
     }
